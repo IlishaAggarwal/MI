76fc12ed728617bd0776cca15ab9ba035056b235
Ilisha Aggarwal <ilishaaggarwal@Ilishas-MacBook-Pro.local>
2022-12-25 19:37:07 +0530

    UI

---
+++ client/src/components/contact.jsx
@@ -0,0 +1,26 @@
+export const Contact = (props) => {
+  return (
+    <div>
+      <div id='contact'>
+        <div className='container'>
+          <div className='contact-item'>
+            <h3>Contact Us</h3>
+            <p>
+              <span>
+                <i className='fa fa-envelope-o'></i> Email
+              </span>{' '}
+              {props.data ? props.data.email : 'loading'}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div id='footer'>
+        <div className='container text-center'>
+          <p>
+            &copy; 2022 MI
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
