6c6e5a3b924988160cde26427c6cab101f0a0d57
Ilisha Aggarwal <ilishaaggarwal@Ilishas-MacBook-Pro.local>
2022-12-31 12:38:22 +0530

    vercel config

--- client/src/components/productpay.jsx
+++ client/src/components/productpay.jsx
@@ -3,37 +3,37 @@ import "../productpay.css";
 import axios from 'axios';
 
 const ProductDisplay = () => (
-	<section>
-	  <div className="product">
-		<img
-		  src="https://i.imgur.com/EHyR2nP.png"
-		/>
-		<div className="description">
-		<h3>5 Stocks</h3>
-		<h5>$100.00</h5>
-		</div>
-	  </div>
-	  {/* <form action="http://localhost:3001/create-checkout-session" method="POST"> */}
-		<button onClick={stripepay} type="submit">
-		  Checkout
-		</button>
-	  {/* </form> */}
-	</section>
-  );
-  
-  const stripepay = async e => {
-    e.preventDefault();
-    await axios.post("http://localhost:3000/create-checkout-session");
-  };
+  <section>
+    <div className="product">
+      <img
+        src="https://i.imgur.com/EHyR2nP.png"
+      />
+      <div className="description">
+        <h3>5 Stocks</h3>
+        <h5>$100.00</h5>
+      </div>
+    </div>
+    {/* <form action="http://localhost:3001/create-checkout-session" method="POST"> */}
+    <button onClick={stripepay} type="submit">
+      Checkout
+    </button>
+    {/* </form> */}
+  </section>
+);
 
-  const Message = ({ message }) => (
-	<section>
-	  <p>{message}</p>
-	</section>
-  );
+const stripepay = async e => {
+  e.preventDefault();
+  await axios.post("http://localhost:3000/create-checkout-session");
+};
+
+const Message = ({ message }) => (
+  <section>
+    <p>{message}</p>
+  </section>
+);
 
 const ProductPay = () => {
-	const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     // Check to see if this is a redirect back from Checkout
