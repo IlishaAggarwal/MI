eede64daffce06b78fa982cf77083d1394199c43
IlishaAggarwal <ilisha.aggarwal30@gmail.com>
2023-01-13 08:44:36 +0530

    update

--- client/src/components/payment.jsx
+++ client/src/components/payment.jsx
@@ -16,13 +16,16 @@ export const Payment = (props) => {
       }),
     })
       .then(res => {
+        console.log("1");
         if (res.ok) return res.json()
         return res.json().then(json => Promise.reject(json))
       })
       .then(({ url }) => {
+        console.log("2");
         window.location = url
       })
       .catch(e => {
+        console.log("3");
         console.error(e.error)
       })
 
--- server/server.js
+++ server/server.js
@@ -7,28 +7,23 @@ app.use(express.json())
 app.use(express.static('public'));
 app.use(
   cors({
-    origin: '*',
+    origin: 'www.elanest.xyz',
         methods: 'GET,PUT,DELETE,POST,PATCH',
         allowedHeaders: 'Content-Type'
   })
 )
 
-app.all("/*", function (req,res,next){
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-  if (req.method="OPTIONS"){
-    res.status(200).end();
-  }
-  else{
-    next();
-  }
+// app.all("/*", function (req,res,next){
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+//   if (req.method="OPTIONS"){
+//     res.status(200).end();
+//   }
+//   else{
+//     next();
+//   }
   
-});
-
-app.get('/ping',(req,res)=>{
-  res.send("helloworld");
-})
-
+// });
 
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
 
