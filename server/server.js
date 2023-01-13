8613c5a9b9d33b20322f81901bac0ca1a8fc8b7d
IlishaAggarwal <ilisha.aggarwal30@gmail.com>
2023-01-13 09:03:35 +0530

    update

--- server/server.js
+++ server/server.js
@@ -13,17 +13,17 @@ app.use(
   })
 )
 
-// app.all("/*", function (req,res,next){
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-//   if (req.method="OPTIONS"){
-//     res.status(200).end();
-//   }
-//   else{
-//     next();
-//   }
+app.all("/*", function (req,res,next){
+  res.header("Access-Control-Allow-Origin", "www.elanest.xyz");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  if (req.method="OPTIONS"){
+    res.status(200).end();
+  }
+  else{
+    next();
+  }
   
-// });
+});
 
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
 
