77196afeb390fef6c3bbacf365c9f551abf99c2b
IlishaAggarwal <ilisha.aggarwal30@gmail.com>
2023-01-13 09:37:08 +0530

    update

--- server/server.js
+++ server/server.js
@@ -7,14 +7,14 @@ app.use(express.json())
 app.use(express.static('public'));
 app.use(
   cors({
-    origin: 'https://www.elanest.xyz',
+    origin: '*',
         methods: 'GET,PUT,DELETE,POST,PATCH',
         allowedHeaders: 'Content-Type'
   })
 )
 
 app.all("/*", function (req,res,next){
-  res.header("Access-Control-Allow-Origin", "https://www.elanest.xyz");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
   if (req.method="OPTIONS"){
     res.status(200).end();
