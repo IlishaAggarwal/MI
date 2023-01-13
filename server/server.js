5a8636abcb507689fb8afccffa025289a360099e
IlishaAggarwal <ilisha.aggarwal30@gmail.com>
2023-01-13 10:47:03 +0530

    update

--- server/server.js
+++ server/server.js
@@ -17,13 +17,13 @@ app.all("/*", function (req,res,next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
   if (req.method="OPTIONS"){
-    return res.status(200).json(({ body: "OK" }))
+    res.status(200).end();
+  }
+  else{
+    next();
   }
   
-  
 });
-
-
 
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
 
