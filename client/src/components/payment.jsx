114c2cc0e3de2b5384693d87717e6bdfbfbe9bf2
IlishaAggarwal <ilisha.aggarwal30@gmail.com>
2023-01-15 13:26:26 +0530

    digitalocean

--- client/src/components/payment.jsx
+++ client/src/components/payment.jsx
@@ -3,7 +3,7 @@ import Card from 'react-bootstrap/Card';
 
 export const Payment = (props) => {
   const PaymentFor15 = () => {
-    fetch("https://mi-backend.vercel.app/create-checkout-session-15", {
+    fetch("http://localhost:3001/create-checkout-session-15", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
