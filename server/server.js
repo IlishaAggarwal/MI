1c942067827534e51da58ea2e83444aa285851ab
IlishaAggarwal <ilisha.aggarwal30@gmail.com>
2023-01-18 01:17:21 +0530

    render

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express()
 const cors = require("cors")
 app.use(express.json())
 app.use(express.static('public'));
-app.use(cors());
+
 // app.use(
 //   cors({
 //     origin: 'http://localhost:3000',
